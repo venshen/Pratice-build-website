458e2698794f7f3308d050641c980de75fdd459d
xiongde shen <xiongde.shen@outlook.com>
2022-05-25 17:10:50 +0800

    delete router

--- src/App.js
+++ src/App.js
@@ -2,30 +2,16 @@ import './App.css';
 import Home from './Home'
 import Header from './Header';
 import Footer from './Footer';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SearchPage from './SearchPage';
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
+    
         <Header />
-
-        <Switch>
-          <Route path="/search">
-            <SearchPage />
-          </Route>
-
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-        
+        <Home />
         <Footer />
-      </Router>
-        
-        
-        
     </div>
   );
 }
